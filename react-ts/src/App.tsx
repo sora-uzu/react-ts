f5aef151e1d230d3a1cc097feeaccc9f09099373
sora_uzu <naruto07170717@gmail.com>
2024-10-01 22:01:32 +0900

    567

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,16 @@
 import Todos from "./components/Todos";
 import "./App.css";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("learn React"), new Todo("learn TypeScript")];
+
+  const addTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -9,6 +9,7 @@ const NewTodo = () => {
     if (enteredText.trim().length === 0) {
       return;
     }
+    props.onAddTodo(enteredText);
   };
   return (
     <form onSubmit={submitHandler}>
